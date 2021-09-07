eb5efeaddecf4d4d9a343f6bcf03069c417a4e94
--replace-all <campbellmcgavin@gmail.com>
2021-09-06 22:31:18 -0600

    Completed Modules 1-6

--- Javascript/06-modal/setup/app.js
+++ Javascript/06-modal/setup/app.js
@@ -1,4 +1,14 @@
 // select modal-btn,modal-overlay,close-btn
+const modalButton = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeButton = document.querySelector(".close-btn");
+
 // listen for click events on modal-btn and close-btn
+modalButton.addEventListener("click", function () {
+    modalOverlay.classList.toggle("open-modal");
+});
+closeButton.addEventListener("click", function () {
+    modalOverlay.classList.toggle("open-modal");
+});
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
--- Javascript/README.md
+++ Javascript/README.md
@@ -1,3 +1,7 @@
+## To see the code that was written as the "cheat sheet" go to the "Final" folder in each example folder.
+
+## To see the code that I wrote, based on the tutorial, navigate to setup folder for each example and open any of the ".js" files.
+
 ## You can see all projects in action here
 
 [Projects](https://www.vanillajavascriptprojects.com/)
